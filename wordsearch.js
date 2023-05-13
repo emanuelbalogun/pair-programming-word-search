e58aabce2e7db510800e71bcdf18cdec3f729e41
Emmanuel Balogun <emanuelbalogun@gmail.com>
2023-05-13 17:23:22 -0500

    Implemented wordsearch pair programing with Prestin Liu

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,54 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = function(matrix) {
+  // length of overall array x
+  // length of each internal array y
+  // array[y][x]
+  let result = [];
+  let x = matrix.length;
+  let y = matrix[0].length;
 
-module.exports = wordSearch
+  for (let j = 0; j < y; j++) {
+    const newRow = [];
+    for (let i = 0; i < x; i++) {
+      newRow.push(matrix[i][j]);
+    }
+    result.push(newRow);
+  }
+  return result;
+};
+
+const wordSearch = (letters, word) => {
+  let transposedLetters = transpose(letters);
+  let vertialJoin = transposedLetters.map((ls) => ls.join(""));
+
+  for (let v of vertialJoin) {
+    if (v.includes(word)) return true;
+  }
+
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  console.log(horizontalJoin);
+
+  for (let h of horizontalJoin) {
+    if (h.includes(word)) return true;
+  }
+
+  return false;
+};
+
+console.log(
+  wordSearch(
+    [
+      ["A", "W", "C", "F", "Q", "U", "A", "L"],
+      ["S", "E", "I", "N", "F", "E", "L", "D"],
+      ["Y", "F", "C", "F", "Q", "U", "A", "L"],
+      ["H", "R", "J", "T", "E", "V", "R", "G"],
+      ["W", "A", "C", "S", "Y", "E", "R", "L"],
+      ["B", "N", "R", "E", "N", "E", "Y", "B"],
+      ["U", "K", "T", "W", "A", "P", "A", "I"],
+      ["O", "D", "C", "A", "K", "U", "A", "S"],
+      ["E", "Z", "K", "F", "Q", "U", "A", "L"],
+    ],
+    "FRANK"
+  )
+);
+
+module.exports = wordSearch;
